221a0cad1891a06d5354dff906745fd5ad38d225
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2021-11-05 14:17:03 +0700

    first commit

---
+++ src/routes/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+
+    render() {
+        const { isLoggedIn } = this.props;
+        // nếu chưa login thì redirect đến /login, lohin rồi thì redirect đến /system/user-manage
+        let linkToRedirect = isLoggedIn ? '/system/user-manage' : '/login';
+
+        return (
+            <Redirect to={linkToRedirect} />
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.admin.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
